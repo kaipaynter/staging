3546799ddf010c87ab399c7c8f30570e9df26d45
beepa <admin@beartigerproductions.com>
2021-03-04 22:37:36 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-be066bb0033229c77748.js"
+    "url": "webpack-runtime-9b6774578f2e692e6fec.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c761f83c98217eb453eb8e6baf06ac2c"
+    "revision": "fade2b8bd57d30656f660b66cb7bcf1f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
