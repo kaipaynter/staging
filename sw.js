475f1e18058f96ad836fd2515212453bf41f9c17
beepa <admin@beartigerproductions.com>
2021-04-13 07:59:16 +0200

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-305b3707783ccc9d7ca6.js"
   },
   {
-    "url": "app-13d6e4c2c625cd35f816.js"
+    "url": "app-595a808bbb0f96a4ed95.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c8555e6c3f087a679dfb1d150afed751"
+    "revision": "3e9b1af4e75e8be85058784f0174958b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-13d6e4c2c625cd35f816.js`))) {
+  if (!resources || !(await caches.match(`/app-595a808bbb0f96a4ed95.js`))) {
     return await fetch(event.request)
   }
 
