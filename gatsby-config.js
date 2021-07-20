4eeeadc8ed231fdd68d1ae14efc39aadf71fafc5
beepa <admin@beartigerproductions.com>
2021-07-20 09:45:07 +0200

    committing update progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,18 +63,18 @@ module.exports = {
     //     },
     //   },
     // },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/assets/images/logo2.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/logo2.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
