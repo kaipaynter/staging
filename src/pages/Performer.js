31ebcfdf3354cdb9166517e61bf3c7457046de6e
beepa <admin@beartigerproductions.com>
2021-08-24 09:57:38 +0200

    added biod and videos

--- src/pages/Performer.js
+++ src/pages/Performer.js
@@ -1,49 +1,52 @@
-import React from 'react';
+import React from 'react'
 
-import Layout from '../components/Layout';
-import pic1 from '../assets/images/01.jpg';
+import Layout from '../components/Layout'
+
+import bio from '../../static/Kai_Paynter.pdf'
 
 const IndexPage = () => (
   <Layout>
     <div id="main">
       <div className="inner">
-        <h1>Paynter Performance</h1>
-        <span className="image main">
-          <img src={pic1} alt="" />
-        </span>
-        <p>
-          Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis
-          in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis
-          sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit
-          amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat.
-          Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna
-          ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien
-          risus, commodo eget turpis at, elementum convallis elit. Pellentesque
-          enim turpis, hendrerit tristique.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
-          rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh
-          porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc
-          ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit
-          sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris,
-          fringilla in aliquam at, euismod in lectus. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. In non lorem sit amet elit placerat maximus. Pellentesque
-          aliquam maximus risus, vel venenatis mauris vehicula hendrerit.
-        </p>
-        <p>
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent
-          rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo
-          mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget
-          turpis at, elementum convallis elit. Pellentesque enim turpis,
-          hendrerit tristique lorem ipsum dolor.
-        </p>
+        <h1>Performer</h1>
+
+        <object
+          data={bio}
+          type="application/pdf"
+          frameborder="1"
+          scrolling="auto"
+          height="700"
+          width="850"
+        >
+          <embed src={bio} type="application/pdf" />
+        </object>
+
+        <br />
+
+        <iframe
+          width="802"
+          height="379"
+          title="Youtube"
+          src="https://www.youtube.com/embed/2cRdZOeAh74"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+
+        <br />
+        <br />
+
+        <iframe
+          src="https://player.vimeo.com/video/589826181?h=da06cbe8cb"
+          width="802"
+          height="379"
+          frameborder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
