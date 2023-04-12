d61584c29e1a6e50449dd971eb2770fba5a39db8
kaipaynter <admin@beartigerproductions.com>
2023-04-12 11:51:20 +0000

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><span>PAYNTER PERFORMANCE</span></Link>
+        <Link to="/" className="logo"><span>HOME</span></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
--- src/pages/teaching_and_training.js
+++ src/pages/teaching_and_training.js
@@ -6,14 +6,6 @@ const TeachingAndTraining = (props) => (
   <Layout>
     <div id="main" className="alt noFooter">
       <section id="one">
-        <p>
-          Paynter Performance has everything you need to take your performance technique to record heights. We have all the tools — all in one place and we guarantee results.
-          <br /> 
-          Resources are available in four editions: 1:1, workshops/group instruction, in-person (limited) and online.
-          <br />
-          Looking for more info about the products and services Paynter Performance offers? Get in touch with us below for a comprehensive list.
-        </p>
-        <br />
         <img class="image fit"
           src={teaching_and_training}
           alt="Teaching and Training"
